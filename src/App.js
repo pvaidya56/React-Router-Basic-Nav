2ae398ef3eb6d0318c055088296cb3bde1b28254
Priya Vaidya <pvaidya56@gmail.com>
2019-10-02 20:12:10 -0700

    Finshed setting up navigation, reached mvp

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => (
     <Navigation />
     <Route exact path="/" component={Home} />
     <Route path="/about" component={About} />
-    <Route path="/contact" component={Navigation} />
+    <Route path="/contact" component={Contact} />
   </div>
 );
 
